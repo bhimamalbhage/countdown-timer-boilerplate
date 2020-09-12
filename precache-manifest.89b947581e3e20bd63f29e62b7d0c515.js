3550e67748f3b8fb96724def6a8bbe5e2faa1687
Bhimashankar <bhimamalbhage@hotmail.com>
2020-09-12 23:25:13 +0530

    Updates

---
+++ precache-manifest.89b947581e3e20bd63f29e62b7d0c515.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d196a687bda26e18fdb3204b725f907b",
+    "url": "/countdown-timer-boilerplate/index.html"
+  },
+  {
+    "revision": "7a862f5d9eda8814cc35",
+    "url": "/countdown-timer-boilerplate/static/css/main.83546c7c.chunk.css"
+  },
+  {
+    "revision": "1c2b9aee36ef8aacc3f3",
+    "url": "/countdown-timer-boilerplate/static/js/2.e2b9d09a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown-timer-boilerplate/static/js/2.e2b9d09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a862f5d9eda8814cc35",
+    "url": "/countdown-timer-boilerplate/static/js/main.49479075.chunk.js"
+  },
+  {
+    "revision": "3b9a2e4c3d48c28dd7d1",
+    "url": "/countdown-timer-boilerplate/static/js/runtime-main.6ef87fe9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/countdown-timer-boilerplate/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b12a435b74e3aacf0ae432137e768773",
+    "url": "/countdown-timer-boilerplate/static/media/whitebg.b12a435b.jpg"
+  }
+]);
